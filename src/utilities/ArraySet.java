c186be15d7c4fa9ebfea436beecfd35ecca332c6
davent4 <aventsister4@gmail.com>
2024-01-16 17:34:35 -0500

    Untested Complete ArraySet
    All the method stubs are now written, although this class is untested and unlikely to work as intended at this stage of development. It's completely written, but as a starting place.

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -1,4 +1,12 @@
 package utilities;
+/**
+ *  An ordered data structure that guarantees uniqueness 
+ *  
+ * <p>Bugs: no tests yet so no clue plus its not done
+ *
+ * @author Sage DeVore and Della Avent
+ * @date 01/19/2024
+ */
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,153 +26,199 @@ public class ArraySet<E> implements List<E>, Set<E>
 	}
 
 	@Override
-	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean add(E e) 
+	{
+		if(contains(e)) return false;
+		_list.add(e);
+		return true;
 	}
 
 	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
+	public void add(int index, E element) 
+	{
+		if(!contains(element)) _list.add(index, element);
 	}
 
+	@Override
+	public boolean addAll(Collection<? extends E> c) 
+	{
+		//TODO idk how to do for each loop like this, no clue if it's right
+		for(E e : c) {
+			if(contains(e)) return false;
+		}
+		_list.addAll(c);
+		return true;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) 
+	{
+		//TODO see method above
+		for(E e : c) {
+			if(contains(e)) return false;
+		}
+		_list.addAll(index, c);
+		return true;
+	}
+
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _list.size();
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return _list.isEmpty();
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		return _list.contains(o);
+		//TODO we could prob make more efficient if it's sorted but idk
 	}
 
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		return _list.containsAll(c);
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
+	public Iterator<E> iterator() {
+		return _list.iterator();
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
+	public Object[] toArray() {
+		return _list.toArray();
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
-	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+	public <T> T[] toArray(T[] a) {
+		return _list.toArray(a);
 	}
 
+	/** (as of now)
+	 * calls a delegate method
+	 */
 	@Override
-	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean remove(Object o) {
+		/*TODO do we need to make this one more efficient bc sorted? but removing
+		 * a specific object will still leave it sorted
+		 */
+		return _list.remove(o);
 	}
 
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.remove(index);
 	}
 
+	/** TODO (as of now)
+	 * calls a delegate method
+	 */
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// same question as other remove
+		return _list.removeAll(c);
+	}
+
+	/** TODO (as of now)
+	 * calls a delegate method
+	 */
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		//does it need to be sorted again after this?
+		return _list.retainAll(c);
+	}
+
+	/**
+	 * calls a delegate method
+	 */
+	@Override
+	public void clear() {
+		_list.clear();
+	}
+
+	/**
+	 * calls a delegate method
+	 */
+	@Override
+	public E get(int index) {
+		//TODO we could prob make more efficient since it's sorted?? do we care??
+		return _list.get(index);
+	}
+
+	/**
+	 * calls a delegate method
+	 */
+	@Override
+	public E set(int index, E element) {
+		return _list.set(index, element);
+	}
+
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return _list.indexOf(o);
 	}
 
+	/** (right now)
+	 * calls a delegate method
+	 */
 	@Override
 	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		//TODO redundant bc its a set; should it throw exception?
+		return _list.lastIndexOf(o);
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.listIterator();
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.listIterator(index);
 	}
 
+	/**
+	 * calls a delegate method
+	 */
 	@Override
 	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.subList(fromIndex, toIndex);
 	}
 
 
